0fb12b2460816f3f8b8834f804bf865543d736b1
Varalakshmi C <varalakshmi.d.chinthakunta@gmail.com>
2024-02-28 14:18:32 +0530

    Added apis for Authentication using JWT tokens

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,20 +1,42 @@
-module.exports = mongoose => {
-    var schema = mongoose.Schema(
-      {
-        name: String,
-        role: String,
-        password: String
-      },
-      { timestamps: true }
-    );
+// module.exports = mongoose => {
+//     var schema = mongoose.Schema(
+//       {
+//         name: String,
+//         role: String,
+//         password: String
+//       },
+//       { timestamps: true }
+//     );
   
-    schema.method("toJSON", function() {
-      const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
-      return object;
-    });
+//     schema.method("toJSON", function() {
+//       const { __v, _id, ...object } = this.toObject();
+//       object.id = _id;
+//       return object;
+//     });
   
-    const User = mongoose.model("Users", schema);
-    return User;
-  };
+//     const User = mongoose.model("Users", schema);
+//     return User;
+//   };
   
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,25 +1,63 @@
+// module.exports = app => {
+//     const user = require("../controllers/user.controller.js");
+  
+//     var router = require("express").Router();
+  
+//     // Create a new User
+//     router.post("/", user.create);
+
+//     // Retrieve all Users
+//    router.get("/", user.findAll);
+
+//    // Retrieve a single user with id
+//    router.get("/:id", user.findOne);
+
+//     // Update a User with id
+//    router.put("/:id", user.update);
+
+//   // Delete a User with id
+//   router.delete("/:id", user.delete);
+
+//   // Create a new User
+//   router.delete("/", user.deleteAll);
+
+//     app.use("/api/users", router);
+// };  
 module.exports = app => {
-    const user = require("../controllers/user.controller.js");
+  const express = require('express'); 
+  const router = express.Router();
+  const jwt = require('jsonwebtoken');
+  const bcrypt = require('bcryptjs');
+  const User = require('../models/user.model');
   
-    var router = require("express").Router();
   
-    // Create a new User
-    router.post("/", user.create);
-
-    // Retrieve all Users
-   router.get("/", user.findAll);
-
-   // Retrieve a single user with id
-   router.get("/:id", user.findOne);
-
-    // Update a User with id
-   router.put("/:id", user.update);
-
-  // Delete a User with id
-  router.delete("/:id", user.delete);
-
-  // Create a new User
-  router.delete("/", user.deleteAll);
-
-    app.use("/api/users", router);
-};  
+  router.post('/register', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const newUser = new User({ email, password });
+      await newUser.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      res.status(400).json({ message: 'Registration failed' });
+    }
+  });
+  
+  router.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+  
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, 'secret_key', { expiresIn: '1h' });
+      res.json({ token });
+    } catch (error) {
+      res.status(500).json({ message: 'Login failed' });
+    }
+  });
+  
+  module.exports = router;
+  app.use("/api/users", router);
+  };  
