6d95a585369a862572f8ab04239b4a9d7f9a32ed
Varalakshmi C <varalakshmi.d.chinthakunta@gmail.com>
2024-03-12 16:29:48 +0530

    added environment variable

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: "mongodb+srv://varalakshmidchinthakunta:jy2M1EtgEy0BTTUr@cluster0.5vqqwmf.mongodb.net/quizzify?retryWrites=true&w=majority"
+  //url: "mongodb+srv://varalakshmidchinthakunta:jy2M1EtgEy0BTTUr@cluster0.5vqqwmf.mongodb.net/quizzify?retryWrites=true&w=majority"
+
+  url: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.5vqqwmf.mongodb.net/quizzify?retryWrites=true&w=majority`
 };
